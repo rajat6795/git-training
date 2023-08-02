69277039817b5878d8d6323e94a46c999a9ff47c
rajat6795 <rajat.mishra6795@gmail.com>
2023-08-02 15:39:07 +0530

    conflict 2 changes

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("Git training with merge conflict")
+	fmt.Println("conflicting changes Git training")
 	fmt.Println("Git checkout")
 	fmt.Println("Git push")
+	fmt.Println("Git amend")
+	fmt.Println("Git merge")
+	fmt.Println("Git branch")
 }
